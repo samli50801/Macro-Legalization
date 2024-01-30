b279b4f9b491a5dd0deae14045e774b6ecbc09c0
Sam Lee <57612267+samli50801@users.noreply.github.com>
2024-01-30 12:57:13 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,19 @@
 #include "cluster.h"
 #include "CG.h"
 #include "bufferLegalizer.h"
-#include <time.h> // includes clock_t and CLOCKS_PER_SEC
+#include <time.h>
 
-//ps_context *context; // Draw Design
 Parser parser;
-
-map<string, Macro*> macro;					// All Macro
-
-vector<Component*> comp;					// All component
-
+map<string, Macro*> macro;			// All Macro
+vector<Component*> comp;			// All component
 unordered_set<Component*> free_space;		// All the space which can place standard cell
 unordered_set<Component*> not_free_space;	// All the space which can't place standard cell
 vector<Component*> boundaryComp;
 vector<Component*> illegal;
-
-vector<Bound*> bound;						// Segments of the design boundary
-
-void plot(Rectangle);
-											
+vector<Bound*> bound;				// Segments of the design boundary
+void plot(Rectangle);										
 vector<vector<Component*>> overlapGroup;	// overlapped group for corner-stitching
-vector<bc::Group*> groups;					// clustering groups
-
-bool isInOverlapGroup(Component* target) {
-	for(vector<vector<Component*>>::iterator it = overlapGroup.begin(); it != overlapGroup.end(); ++it){
-		for (vector<Component*>::iterator i = (*it).begin(); i != (*it).end(); ++i){
-			if (target == *i)
-				return true;
-		}
-	}
-	return false;
-}
+vector<bc::Group*> groups;			// clustering groups
 vector<Component*> deletePoint;
 vector<Component*> movedMacro;
 
@@ -47,8 +30,8 @@ int main(int argc, char* argv[])
 	double duration_sec;
 	fstream Log("log.txt", ios::out);
 
-    fstream output;
-    output.open(argv[4], ios::out);
+    	fstream output;
+    	output.open(argv[4], ios::out);
 
 	parser.readLef("testbench/" + string(argv[2]), macro);
 	parser.readDef("testbench/" + string(argv[1]), output, macro, comp);
@@ -59,8 +42,8 @@ int main(int argc, char* argv[])
 	bound = parser.getBound();
 
 	/* preprcessor: bounding box and boundary tiles construction */
-    Preprocessor preprocessor(bound, parser);
-    preprocessor.run();
+    	Preprocessor preprocessor(bound, parser);
+    	preprocessor.run();
 	//preprocessor.buildBoundaryComponent();
 	boundaryComp = preprocessor.getBoundaryComp();
 
@@ -69,52 +52,45 @@ int main(int argc, char* argv[])
 	// For corner-stitching
 	Force_Directed fd(parser._mcsbmc * parser.dbuPerMicron);
 	fd.move_macro_inside_boundary();	// Move macros ounside the design into the nearest boundary
-	start = clock();	// time
+	start = clock();
 	fd.do_forceDirected();
-	end = clock();	// time
-	duration_sec = double(end-start) / CLOCKS_PER_SEC;	// time
+	end = clock();
+	duration_sec = double(end-start) / CLOCKS_PER_SEC;
 	Log << "Force-directed: " << duration_sec << "sec" << endl;
 
 	plot(preprocessor.getBoundingRect());
-	//plot(preprocessor.getBoundingRect());
 
 	cg::CG cg(comp, boundaryComp, parser);
 	cg.legalize();
 	cg.optBufferArea();
-	deletePoint = cg.deletedPoint;		// Debug
+	deletePoint = cg.deletedPoint;
 
-	//plot(preprocessor.getBoundingRect());
+    	Legalizer legalizer(comp,
+        preprocessor.getPlane(), 
+        preprocessor.getBoundingRect(),
+        parser);
 
-    Legalizer legalizer(comp,
-            preprocessor.getPlane(), 
-            preprocessor.getBoundingRect(),
-            parser);
-
-	start = clock();	// time
+	start = clock();
 	legalizer.legalize(deletePoint);
-	end = clock();	// time
-	duration_sec = double(end-start) / CLOCKS_PER_SEC;	// time
+	end = clock();
+	duration_sec = double(end-start) / CLOCKS_PER_SEC;
 	Log << "corner-stitch: " << duration_sec << "sec" << endl;
 	plot(preprocessor.getBoundingRect());
-	//plot(preprocessor.getBoundingRect());
 
 	buffer::BufferLegalizer bufferLegalizer(bound, comp, boundaryComp, parser);
 	bufferLegalizer.legalize();
 	illegal = bufferLegalizer.getIllegal();
 	free_space = bufferLegalizer.getFreeSpace();
 	not_free_space = bufferLegalizer.getDeadSpace();
-	deletePoint = bufferLegalizer.getDeletedComp();		// Debug
 	cout << "illegal: " << illegal.size() << endl;
 	plot(preprocessor.getBoundingRect());
 
-    preprocessor.resetPlane();
-    legalizer.needCheckBuffer();
-    legalizer.buildBufferPlane(free_space, preprocessor.getBufferPlane());
-    legalizer.setPlane(preprocessor.getPlane());
-    legalizer.legalize(deletePoint);
+	preprocessor.resetPlane();
+	legalizer.needCheckBuffer();
+	legalizer.buildBufferPlane(free_space, preprocessor.getBufferPlane());
+	legalizer.setPlane(preprocessor.getPlane());
+	legalizer.legalize(deletePoint);
 	cout << "comp size: " << comp.size() << endl;
-    //legalizer.plot();
-    //legalizer.bufferPlot();
 
 	/* gnuplot */
 	plot(preprocessor.getBoundingRect());
@@ -122,10 +98,11 @@ int main(int argc, char* argv[])
 	buffer::BufferLegalizer bufferLegalizer2(bound, comp, boundaryComp, parser);
 	bufferLegalizer2.legalize();
 	illegal = bufferLegalizer2.getIllegal();
-    if(illegal.empty())
+    	if(illegal.empty())
 		cout << "Successfully buffer legalize\n";
-    else
+   	else
 		cout << "Fail to legalize\n";
+	
 	/*check if the final placement is legal (no overlappings) */
 	fd.generate_overlap_group();		// Generate overlapped group
 	overlapGroup = fd.getGroup();
@@ -136,9 +113,7 @@ int main(int argc, char* argv[])
 
 	parser.recoverCoordinateSystem(comp);
 	parser.writeDef(output, comp);
-
-    parser.score_evaluation(comp, not_free_space);
-
+    	parser.score_evaluation(comp, not_free_space);
 
 	return 0;
 }
@@ -163,31 +138,7 @@ void plot(Rectangle boundingRect){
 	"#90ee99", "#f055f0", "#ffd700", "#00ff00", "#006400", "#0000ff", "#000080", "#00ffff", "#ff00ff", "#00ced1",
 	"#ff1493", "#ff7f50", "#ff4500", "#9400d3", "#905040", "#556b2f", "#801400", "#804080", "#8060ff", "#808000"
 	};
-	/*size_t colorIndex = 0;
-	for (size_t i = 0; i < groups.size(); ++i) {
-		if (groups[i] != NULL) {
-			for (size_t j = 0; j < groups[i]->_members.size(); ++j) {
-				Component c = *groups[i]->_members[j];
-                string NodeName = c.getName();
-	    		int x0 = c.get_ll_x();
-	    		int y0 =  c.get_ll_y();
-				int x1 = c.get_ur_x();
-				int y1 = c.get_ur_y();
-				int midX = (x0+x1)/2;
-				int midY = (y0+y1)/2;
-				if (groups[i]->_members.size() == 1) {
-					outgraph << "set object " << index << " rect from "
-					<< x0 << "," << y0 << " to " << x1 << "," << y1 << " fs empty border lc rgb '#000000'\n";
-					index++;
-				}else {
-					outgraph << "set object " << index << " rect from " 
-		  			<< x0 << "," << y0 << " to " << x1 << "," << y1 << " fs empty border lc rgb '" << ColorValues[colorIndex % 40] << "'\n";
-					index++;
-				}
-            }
-			colorIndex++;
-		}
-	}*/
+	
  	for (vector<Component*>::iterator i = comp.begin(); i != comp.end(); i++) {
 		string NodeName = (*i)->getName();
 	    int x0 = (*i)->get_ll_x();
@@ -214,24 +165,7 @@ void plot(Rectangle boundingRect){
 		
 		index++;
 	}
-	/*size_t colorIndex = 5;
-	int c = 1;
-	for (vector<Component*>::iterator i = movedMacro.begin(); i != movedMacro.end(); i++) {
-		string NodeName = (*i)->getName();
-	    int x0 = (*i)->get_ll_x();
-	    int y0 =  (*i)->get_ll_y();
-		int x1 = (*i)->get_ur_x();
-		int y1 = (*i)->get_ur_y();
-		int midX = (x0+x1)/2;
-		int midY = (y0+y1)/2;
-		outgraph << "set object " << index << " rect from " 
-		  		<< x0 << "," << y0 << " to " << x1 << "," << y1 << " fs empty border lc rgb '" << ColorValues[colorIndex % 40] <<  "'\n";
-			//<< "set label " << "\"" << NodeName << "\"" << " at " << midX << "," << midY << " center " << "font \",8\"\n";
 	
-		index++;
-		if (c%2 == 0) colorIndex++;
-		c++;
-	}*/
 	for (vector<Component*>::iterator i = deletePoint.begin(); i != deletePoint.end(); i++) {
 		string NodeName = (*i)->getName();
 	    int x0 = (*i)->get_ll_x();
@@ -290,31 +224,8 @@ void plot(Rectangle boundingRect){
 		index++;  
 	}
 
-	/*for (auto it = free_space.begin(); it != free_space.end(); ++it) {
-		int x0 = (*it)->get_ll_x();
-	    int y0 =  (*it)->get_ll_y();
-		int x1 = (*it)->get_ur_x();
-		int y1 = (*it)->get_ur_y();
-		int midX = (x0+x1)/2;
-		int midY = (y0+y1)/2;
-		outgraph << "set object " << index << " rect from " 
-		  	<< x0 << "," << y0 << " to " << x1 << "," << y1 << " fc rgb 'green'\n";
-	
-		index++;  
-	}*/
-
 	fstream outline("line", ios::out);
 
-	/* draw boundary*/
-	/*for (size_t i = 0; i < parser.dieArea.size(); ++i) {
-		if (i < parser.dieArea.size() - 1) {
-			outline << parser.dieArea[i].first << " " << parser.dieArea[i].second << endl;
-			outline << parser.dieArea[i + 1].first << " " << parser.dieArea[i + 1].second << endl;
-		} else {
-			outline << parser.dieArea[i].first << " " << parser.dieArea[i].second << endl;
-			outline << parser.dieArea[0].first << " " << parser.dieArea[0].second << endl;
-		}
-	}*/
 	for (vector<Bound*>::iterator i = bound.begin(); i != bound.end(); i++) {
 		if ((*i)->dir == H) {
 			outline << (*i)->start << " " << (*i)->pos << endl;
